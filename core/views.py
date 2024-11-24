f9ea54ef937433fe8a26ab10f16b204095df69f1
Mohammad AsadUllah Siddiqui <80839481+AsadUllahSiddiqui559@users.noreply.github.com>
2024-11-24 18:23:38 +0500

    feat(api): Add teacher-related endpoints for listing, detail, and classes (#3)
    - Implemented `TeacherListView` to fetch all teachers.
    - Added `TeacherDetailView` to retrieve details of a specific teacher by ID.
    - Created `TeacherClassesView` to fetch all classes associated with a teacher.
    - Registered the endpoints:
      - `/api/teachers/` for listing all teachers.
      - `/api/teachers/<id>/` for teacher details.
      - `/api/teachers/classes/` for teacher's classes.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from core.models import Student
-from core.serializers import StudentSerializer, ClassSerializer
+from core.models import Student, Teacher
+from core.serializers import StudentSerializer, ClassSerializer, TeacherSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
@@ -13,7 +13,7 @@ from .serializers import CustomTokenObtainPairSerializer
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
-# get all students
+# students
 class StudentListView(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
@@ -37,4 +37,30 @@ class StudentClassesView(ListAPIView):
         return Response(serializer.data)
       except Student.DoesNotExist:
         return Response({"error": "Student not found"}, status=status.HTTP_404_NOT_FOUND)
+
+# Teachers
+class TeacherListView(ListAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [IsAuthenticated]
+
+class TeacherDetailView(RetrieveAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    permission_classes = [IsAuthenticated]
+
+class TeacherClassesView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+      if request.user.role != 'teacher':
+        return Response({"error": "You are not a teacher"}, status=status.HTTP_400_BAD_REQUEST)
+
+      try:
+        teacher = Teacher.objects.get(user=request.user)
+        classes = teacher.classes.all()
+        serializer = ClassSerializer(classes, many=True)
+        return Response(serializer.data)
+      except teacher.DoesNotExist:
+        return Response({"error": "teacher not found"}, status=status.HTTP_404_NOT_FOUND)
+
 
