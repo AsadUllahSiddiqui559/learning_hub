2483534a650c68cfdbd6db4ff8ed80a7524c5aef
AsadUllahSiddiqui559 <asadsiddiquidev@gmail.com>
2024-11-24 18:11:02 +0500

    feat(api): Add teacher-related endpoints for listing, detail, and classes
    - Implemented `TeacherListView` to fetch all teachers.
    - Added `TeacherDetailView` to retrieve details of a specific teacher by ID.
    - Created `TeacherClassesView` to fetch all classes associated with a teacher.
    - Registered the endpoints:
      - `/api/teachers/` for listing all teachers.
      - `/api/teachers/<id>/` for teacher details.
      - `/api/teachers/classes/` for teacher's classes.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from core.views import StudentListView
-from core.views import StudentDetailView
-from core.views import StudentClassesView
+from core.views import StudentListView, StudentDetailView, StudentClassesView
+from core.views import TeacherListView, TeacherDetailView, TeacherClassesView
 from .views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
@@ -9,12 +8,19 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('api/token/', CustomTokenObtainPairView.as_view()  , name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    # students related apis
-    path('api/students/', StudentListView.as_view(), name='student_list'),
-    path('api/students/<int:pk>/', StudentDetailView.as_view(), name='student_detail'),
-    path('api/students/classes/', StudentClassesView.as_view(), name='student_classes'),
+  # authentication related apis
+  path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+  # students related apis
+  path('api/students/', StudentListView.as_view(), name='student_list'),
+  path('api/students/<int:pk>/', StudentDetailView.as_view(), name='student_detail'),
+  path('api/students/classes/', StudentClassesView.as_view(), name='student_classes'),
+
+  # teachers related apis
+  path('api/teachers/', TeacherListView.as_view(), name='teacher_list'),
+  path('api/teachers/<int:pk>/', TeacherDetailView.as_view(), name='teacher_detail'),
+  path('api/teachers/classes/', TeacherClassesView.as_view(), name='teacher_classes'),
 ]
